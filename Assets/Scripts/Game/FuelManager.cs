1a87f9da7e2f2d5388fad119e15585f2cee41f6b
Unknown <nacho7577@hotmail.com>
2018-05-07 11:32:57 -0300

    Extended the Track & Created the Main Menu Layout

---
+++ Assets/Scripts/Game/FuelManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelManager : MonoBehaviour 
+{
+    private static FuelManager instance;
+    [SerializeField] private float fuelAmount;
+
+    void Awake()
+    {
+        if (Instance == this)
+            Debug.Log("The Fuel Manager has been initialized correctly.", gameObject);
+        else
+            Debug.Log("The level already had a Fuel Manager.", Instance);
+    }
+
+    void Update()
+    {
+        fuelAmount -= Time.deltaTime;
+        if (fuelAmount <= 0)
+            LevelManager.Instance.EndGame();
+    }
+
+    public FuelManager Instance
+    {
+        get
+        {
+            if (!instance)
+                instance = FindObjectOfType<FuelManager>();
+            if (!instance)
+            {
+                GameObject gameObj = new GameObject("Fuel Manager");
+                instance = gameObj.AddComponent<FuelManager>();
+            }
+            return instance;
+        }
+    }
+
+    public float FuelAmount
+    {
+        get { return fuelAmount; }
+    }
+}
