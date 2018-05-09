f0dee305ac65a60d40caadb76ca1fe668c0f8809
Unknown <nacho7577@hotmail.com>
2018-05-09 20:15:33 -0300

    Added Sound Effects for the Coins

--- Assets/Scripts/Collectibles/CoinPickUp.cs
+++ Assets/Scripts/Collectibles/CoinPickUp.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
 
 public class CoinPickUp : MonoBehaviour 
 {
     [SerializeField] private GameObject coinParticles;
+    [SerializeField] private AudioSource pickUpSound;
     [SerializeField] private float particlesDuration;
 
     void OnTriggerEnter(Collider other)
     {
-        ScoreManager.Instance.Score++;
-        GameObject sparks = Instantiate(coinParticles, transform.position, transform.rotation);
-        Destroy(coinParticles.gameObject, particlesDuration);
-        Destroy(gameObject);
+        if (!LevelManager.Instance.GameOver)
+        {
+            ScoreManager.Instance.Score++;
+            pickUpSound.Play();
+            GameObject sparks = Instantiate(coinParticles, transform.position, transform.rotation);
+            Destroy(sparks.gameObject, particlesDuration);
+            Destroy(gameObject, pickUpSound.clip.length);
+            //gameObject.SetActive(false);
+        }
     }
 }
