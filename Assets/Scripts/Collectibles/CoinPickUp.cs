5cfd765c39c1b70efb062f03e3b77ed8b29d85f3
Unknown <nacho7577@hotmail.com>
2018-05-09 19:47:29 -0300

    Created a Particle System for the Coins

--- Assets/Scripts/Collectibles/CoinPickUp.cs
+++ Assets/Scripts/Collectibles/CoinPickUp.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class CoinPickUp : MonoBehaviour 
 {
+    [SerializeField] private GameObject coinParticles;
+    [SerializeField] private float particlesDuration;
+
     void OnTriggerEnter(Collider other)
     {
         ScoreManager.Instance.Score++;
+        GameObject sparks = Instantiate(coinParticles, transform.position, transform.rotation);
+        Destroy(coinParticles.gameObject, particlesDuration);
         Destroy(gameObject);
     }
 }
