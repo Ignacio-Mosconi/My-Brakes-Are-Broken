c250616a33ecd732a5d71c38de75d553adabed10
Unknown <nacho7577@hotmail.com>
2018-05-09 16:15:40 -0300

    Added Scoring Logic

---
+++ Assets/Scripts/Game Management/ScoreManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour 
+{
+    private static ScoreManager instance;
+    private int score;
+
+    void Awake()
+    {
+        if (Instance == this)
+            Debug.Log("The Score Manager has been initialized correctly.", gameObject);
+        else
+            Debug.LogError("The scene already had a Score Manager.", Instance);
+    }
+
+    public static ScoreManager Instance
+    {
+        get
+        {
+            if (!instance)
+                instance = FindObjectOfType<ScoreManager>();
+            if (!instance)
+            {
+                GameObject gameObj = new GameObject("Score Manager");
+                instance = gameObj.AddComponent<ScoreManager>();
+            }
+            return instance;
+        }
+    }
+
+    public int Score
+    {
+        get { return score; }
+    }
+}
